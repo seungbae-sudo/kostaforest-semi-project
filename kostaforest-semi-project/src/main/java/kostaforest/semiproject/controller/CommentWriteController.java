fe19558666e3121adcdf4772ac43747b14833b17
seungbae-sudo <tmdqoek12@gmail.com>
2022-05-06 14:02:17 +0900

    좋아요 버튼 수정 삭제 권한 댓글 기능 수정

--- kostaforest-semi-project/src/main/java/kostaforest/semiproject/controller/CommentWriteController.java
+++ kostaforest-semi-project/src/main/java/kostaforest/semiproject/controller/CommentWriteController.java
@@ -2,6 +2,7 @@ package kostaforest.semiproject.controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import kostaforest.semiproject.model.CommentVO;
 import kostaforest.semiproject.model.CommunityBoardDAO;
@@ -12,13 +13,14 @@ public class CommentWriteController implements Controller {
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		HttpSession session = request.getSession();
+		MemberVO mvo = (MemberVO)session.getAttribute("mvo");
 		String boardNo = request.getParameter("boardNo");
 		String comName = request.getParameter("conName");
 		String comContent = request.getParameter("com_content");
 		CommunityPostVO cmuvo = CommunityBoardDAO.getInstance().findPostByNo(boardNo);
 		cmuvo.setBoardNo(Integer.parseInt(boardNo));
-		MemberVO mvo = new MemberVO();
-		mvo.setComName(comName);
+	
 		CommentVO covo = new CommentVO();
 		covo.setCommentContent(comContent);
 		covo.setCmuvo(cmuvo);
