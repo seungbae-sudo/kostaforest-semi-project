b765e3023b48e2af454e5487c990ce81b0c0aae1
Jiyeon-Park-dev <bakgii@naver.com>
2022-05-06 12:28:08 +0900

    채용 detail에 계약직/ 경력직 보이게 수정함

--- kostaforest-semi-project/src/main/java/kostaforest/semiproject/controller/mainHomtListController.java
+++ kostaforest-semi-project/src/main/java/kostaforest/semiproject/controller/mainHomtListController.java
@@ -1,5 +1,9 @@
 package kostaforest.semiproject.controller;
 
+import java.util.ArrayList;
+import java.util.Date;
+
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -9,6 +13,8 @@ public class mainHomtListController implements Controller {
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		
 		request.setAttribute("List1", CommunityBoardDAO.getInstance().findAllListByCategoryNo("1"));
 		request.setAttribute("List2", CommunityBoardDAO.getInstance().findAllListByCategoryNo("2"));
 		request.setAttribute("List3", CommunityBoardDAO.getInstance().findAllListByCategoryNo("3"));
