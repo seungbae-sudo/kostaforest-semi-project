f479089d58e5dd1885c40beebd53165aeeb38661
seungbae-sudo <tmdqoek12@gmail.com>
2022-05-09 17:46:25 +0900

    커뮤니티 게시판 페이지네이션

--- kostaforest-semi-project/src/main/java/kostaforest/semiproject/controller/CommunityDeleteController.java
+++ kostaforest-semi-project/src/main/java/kostaforest/semiproject/controller/CommunityDeleteController.java
@@ -1,27 +1,23 @@
 package kostaforest.semiproject.controller;
 
-import java.util.ArrayList;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import kostaforest.semiproject.model.CommunityBoardDAO;
-import kostaforest.semiproject.model.CommunityPostVO;
 
 public class CommunityDeleteController implements Controller {
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		String pageNo = request.getParameter("pageNo");
 		String no = request.getParameter("no");
 		String carNo = request.getParameter("carNo");
 		CommunityBoardDAO.getInstance().deleteByBoardNoComment(no);
 		CommunityBoardDAO.getInstance().deletePostByNo(no);
-		ArrayList<CommunityPostVO> list = CommunityBoardDAO.getInstance().findAllPostList(carNo);
 		
 		request.setAttribute("url", "communityList.jsp");
 		request.setAttribute("carNo", carNo);
-		request.setAttribute("cmuList", list);
-		return "layout.jsp";
+		return "redirect:CommunityPostListController.do?carNo="+carNo+"&pageNo="+pageNo;
 	}
 
 }
