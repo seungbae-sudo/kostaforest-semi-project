faf96ed7da9f6aebc80478486f862fcdf8079da9
seungbae-sudo <tmdqoek12@gmail.com>
2022-05-10 11:16:44 +0900

    FAQ 구현

--- kostaforest-semi-project/src/main/java/kostaforest/semiproject/controller/FAQController.java
+++ kostaforest-semi-project/src/main/java/kostaforest/semiproject/controller/FAQController.java
@@ -3,11 +3,14 @@ package kostaforest.semiproject.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import kostaforest.semiproject.model.FAQBoardDAO;
+
 public class FAQController  implements Controller {
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		
+
+		request.setAttribute("faqList", FAQBoardDAO.getInstance().faqList());
 		request.setAttribute("url", "FAQ.jsp");
 		return "layout.jsp";
 	}
--- kostaforest-semi-project/src/main/java/kostaforest/semiproject/model/FAQBoardDAO.java
+++ kostaforest-semi-project/src/main/java/kostaforest/semiproject/model/FAQBoardDAO.java
@@ -4,12 +4,13 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import javax.sql.DataSource;
 
 public class FAQBoardDAO {
 	private static FAQBoardDAO instance = new FAQBoardDAO();
-	private DataSource  dataSource;
+	private DataSource dataSource;
 	private FAQBoardDAO() {
 		this.dataSource = DataSourceManager.getInstance().getDataSource();
 	}
@@ -28,5 +29,27 @@ public class FAQBoardDAO {
 		if (rs != null)
 			rs.close();
 		closeAll(pstmt, con);
+	}
+	public ArrayList<FAQPostVO> faqList() throws SQLException {
+		ArrayList<FAQPostVO> list = new ArrayList<FAQPostVO>();
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			con = dataSource.getConnection();
+			String sql = "SELECT * FROM FAQ";
+			pstmt = con.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			while(rs.next()) {
+				FAQPostVO fvo = new FAQPostVO();
+				fvo.setFaqNo(rs.getInt("faq_no"));
+				fvo.setAsk(rs.getString("ask"));
+				fvo.setAnswer(rs.getString("answer"));
+				list.add(fvo);
+			}
+		}finally {
+			closeAll(rs, pstmt, con);
+		}
+		return list;
 	}
 }
