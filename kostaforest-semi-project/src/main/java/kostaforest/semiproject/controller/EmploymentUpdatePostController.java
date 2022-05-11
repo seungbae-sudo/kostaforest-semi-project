2863ac15c5f371e34d275581018886d4baf3d991
sojungLEEdev <sojung2017083@gmail.com>
2022-05-11 15:36:51 +0900

    review post방식 변경 , employee post방식 변경, layout 변경

--- kostaforest-semi-project/src/main/java/kostaforest/semiproject/controller/EmploymentUpdatePostController.java
+++ kostaforest-semi-project/src/main/java/kostaforest/semiproject/controller/EmploymentUpdatePostController.java
@@ -1,5 +1,6 @@
 package kostaforest.semiproject.controller;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -10,6 +11,9 @@ public class EmploymentUpdatePostController implements Controller {
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		if(request.getMethod().equals("POST")==false)	//post방식이 아니면
+			throw new ServletException(getClass().getName()+"POST방식만 서비스 가능합니다.");  //이거 없으면 url에서 맘대로 삭제하고 그럼
 
 		String no = request.getParameter("no");
 		String title = request.getParameter("title");
--- kostaforest-semi-project/src/main/java/kostaforest/semiproject/controller/ReviewPostUpdateFormController.java
+++ kostaforest-semi-project/src/main/java/kostaforest/semiproject/controller/ReviewPostUpdateFormController.java
@@ -1,6 +1,5 @@
 package kostaforest.semiproject.controller;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -10,9 +9,6 @@ public class ReviewPostUpdateFormController implements Controller {
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		//request.method 확인
-				if(request.getMethod().equals("POST")==false)	//post방식이 아니면
-					throw new ServletException(getClass().getName()+"POST방식만 서비스 가능합니다."); 
 				
 				String no=request.getParameter("no");
 				request.setAttribute("rvo", ReviewBoardDAO.getInstance().findPostByNo(no));
